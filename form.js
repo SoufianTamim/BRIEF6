5e04bd54c0b9b6fe4e22d8cec9a3383029dba665
soufian Tamim <soufiantamim22@gamil.com>
2022-11-18 11:08:52 +0100

    gender and group verification

--- form.js
+++ form.js
@@ -5,53 +5,112 @@ function empty(value) {
   return true;
 }
 
-function checkradio(array, bool) {
-  array.forEach((ele) => {
-    if (ele.checked === true) {
-      bool[0] = true;
-      console.log(bool);
-      return bool;
+// function checkR(radio, group) {
+//   radio.forEach((rd) => {
+//     if (rd.checked === true) {
+//       group[0] = true;
+//       return group;
+//     }
+//   });
+// }
+
+function checkR(array, jee) {
+  array.forEach((rd) => {
+    if (rd.checked === true) {
+       console.log(jee);
+       console.log("troo");
+      jee[0] = true;
+      return jee;
     }
   });
 }
 
+
 const errors = [];
 
 submit.onclick = function (ev) {
-  console.log("clicked");
-  alertbox.style.display = "none";
-  alertbox.innerHTML = "";
+  // alertbox.style.display = "none";
+  // alertbox.innerHTML = "";
   errors.length = 0;
+  ev.preventDefault();
+  
+  let firstname = document.getElementsByName("firstname")[0].value;
+  let lastname = document.getElementsByName("lastname")[0].value;
+  let email = document.getElementsByName("email")[0];
+  let phone = document.getElementsByName("phone")[0];
+  let gender = document.getElementsByName("gender");
+  let group = document.getElementsByName("group")[0].selectedOptions;
+  // let clubs = document.getElementsByName("clubs")[0].selectedOptions;
 
-  let first_name = document.getElementsByName("firstname")[0].value;
-  let last_name = document.getElementById("last_name").value;
-  let email = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
-  let gender = document.getElementById("gender").value;
-  //   let group = document.getElementById("group").value; // =========I should give the html element an Id=== //
+
+
   //   let clubs = document.getElementById("clubs").value;
+
 
   // let phone = "0677846064"
   // let phoneRe = /0[5-7]/g;
+
   // console.log(phone.match(phoneRe));
 
   // let emailRe = /([a-z])\w+.?([a-z])\w+.?([a-z])\w+?([0-9]?)@(gmail|hotmail|yahoo|ofppt).(com|org|net|ma)/g;
 
+
+  //======================= first name validaton =============================//
   let myRegex = /^[a-zA-Z-\s]+$/;
 
-  ev.preventDefault();
-  if (first_name.length === 0) {
-    errors.push("the firstname is required , can't be empty ");
-  } else if (first_name.length > 15) {
-    errors.push("the max length for the firstname is 15 ");
-  } else if (myRegex.test(first_name) === -1) {
-    errors.push("the first name can't contain numbers");
+  if (firstname.length === 0) {
+    errors.push("firstname is required , cannot be empty ");
+  } else if (firstname.length > 15) {
+    errors.push(
+      "first name is too long, it should be less than 15 characters "
+    );
+  } else if (myRegex.test(firstname) === false) {
+    errors.push("first name cannot contain numbers");
   }
 
+  //======================= last name validaton =============================//
+
+  if (lastname.length === 0) {
+    errors.push("last name is required , cannot be empty ");
+  } else if (lastname.length > 15) {
+    errors.push("last name is too long, it should be less than 15 characters ");
+  } else if (myRegex.test(lastname) === false) {
+    errors.push("last  name cannot contain numbers");
+  }
+
+  //======================= gender Validation =============================//
+
+  const genderC = [false];
+
+  checkR(gender, genderC);
+
+  if (genderC[0] === false) {
+    errors.push("Please you have to choose your gender");
+  }
+
+
+  //======================= group Validation =============================//
+
+
+
+
+  // const groupCase = [false];
+
+  // checkR(group, groupCase);
+
+  // if (groupCase[0] === false) {
+  //   errors.push("you must choose your group");
+  // }
+
+
+
+
+
+  //======================= Errors =============================//
   if (errors.length > 0) {
     for (let i = 0; i < errors.length; i++) {
       alertbox.style.display = "block";
-      alertbox.innerHTML += "<strong> error ! </strong>" + errors[i] + "<br>";
+      alertbox.innerHTML += "<strong> ERROR! </strong>" + errors[i] + "<br>";
     }
   }
 };
